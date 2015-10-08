8079f02d36a603b202be63429687f55cc8992b51
wangtao <wangtao@qqing.com>
2015-10-08 15:20:08 +0800

    上传工程

---
+++ WTiOSBasicKit/WTiOSBasicKitTests/WTiOSBasicKitTests.m
@@ -0,0 +1,39 @@
+//
+//  WTiOSBasicKitTests.m
+//  WTiOSBasicKitTests
+//
+//  Created by 王涛 on 15/10/8.
+//  Copyright © 2015年 王涛. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WTiOSBasicKitTests : XCTestCase
+
+@end
+
+@implementation WTiOSBasicKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
